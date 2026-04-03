567dd12412959946fbe00a6c77adfe5eb6539d5b
Abdir <rammadan1213@gmail.com>
2026-04-03 15:52:33 +0300

    Fix inverse filter error and add sub filter for progress bar

--- accounts/templatetags/custom_filters.py
+++ accounts/templatetags/custom_filters.py
@@ -9,3 +9,27 @@ def mul(value, arg):
         return int(value) * int(arg)
     except (ValueError, TypeError):
         return 0
+
+
+@register.filter
+def div(value, arg):
+    try:
+        return int(value) / int(arg) if int(arg) != 0 else 0
+    except (ValueError, TypeError):
+        return 0
+
+
+@register.filter
+def mult(value, arg):
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return 0
+
+
+@register.filter
+def sub(value, arg):
+    try:
+        return float(value) - float(arg)
+    except (ValueError, TypeError):
+        return 0
